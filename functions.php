c44280c05f17b822d06cefa1fd333b988c474b75
ev3rywh3re <jess@funroe.net>
2024-09-02 14:05:53 -0500

    Adding the include for the block experiment plugin to the theme for testing.

--- functions.php
+++ functions.php
@@ -33,6 +33,30 @@ define('JESSE_JAMES_BLOCKS_VERSION', '1.0.0');
  * Add custom blocks for gutenberg
  */
 // require get_stylesheet_directory() . '/assets/blocks/blocks.php';
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function register_block_jesse_james_block_plugin_scaffold() {
+
+  $block_dir = get_stylesheet_directory() . '/assets/blocks/';
+  $block_jess_block_plugin_scaffold_experiments = $block_dir . '/jesse-james-wp-block-plugin-scaffold/jess-block-scaffold-experiments.php';
+
+//  echo " TEST-1:" . $block_dir ;
+//  echo " TEST-2:" . is_dir( $block_dir )  ;
+//	register_block_type( __DIR__ . '/build' );
+
+//  register_block_type( $block_dir );
+
+include_once( $block_jess_block_plugin_scaffold_experiments );
+
+}
+add_action( 'init', 'register_block_jesse_james_block_plugin_scaffold' );
+
+
 
 /**
  * Add pre-styles.css - for resets, etc.
