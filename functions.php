81b4d5a64a74c704ea5eb6516184a321e081c324
ev3rywh3re <jess@funroe.net>
2024-11-19 21:34:54 -0600

    Theme css clean ups with new testing.

--- functions.php
+++ functions.php
@@ -12,38 +12,43 @@ define('JESSE_JAMES_BLOCKS_VERSION', '1.0.0');
 
 
 
-function jesse_james_add_block_template_file_support() {
-  add_theme_support( 'block-templates' );
-  add_theme_support( 'block-template-parts' );
+function jesse_james_add_block_template_file_support()
+{
+    add_theme_support('block-templates');
+    add_theme_support('block-template-parts');
 }
-add_action( 'after_setup_theme', 'jesse_james_add_block_template_file_support' );
+add_action('after_setup_theme', 'jesse_james_add_block_template_file_support');
 
 
 // Allow SVG
-add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+add_filter(
+    'wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes) {
 
-    global $wp_version;
-    if ( $wp_version !== '4.7.1' ) {
-       return $data;
-    }
+        global $wp_version;
+        if ($wp_version !== '4.7.1' ) {
+            return $data;
+        }
   
-    $filetype = wp_check_filetype( $filename, $mimes );
+        $filetype = wp_check_filetype($filename, $mimes);
   
-    return [
+        return [
         'ext'             => $filetype['ext'],
         'type'            => $filetype['type'],
         'proper_filename' => $data['proper_filename']
-    ];
+        ];
   
-  }, 10, 4 );
+    }, 10, 4 
+);
   
-function cc_mime_types( $mimes ){
+function cc_mime_types( $mimes )
+{
     $mimes['svg'] = 'image/svg+xml';
     return $mimes;
 }
-add_filter( 'upload_mimes', 'cc_mime_types' );
+add_filter('upload_mimes', 'cc_mime_types');
   
-function fix_svg() {
+function fix_svg()
+{
     echo '<style type="text/css">
           .attachment-266x266, .thumbnail img {
                width: 100% !important;
@@ -51,68 +56,75 @@ function fix_svg() {
           }
           </style>';
 }
-add_action( 'admin_head', 'fix_svg' );
+add_action('admin_head', 'fix_svg');
 
 /**
  * Enqueue React scripts.
  * Includes wp-element dependency. 
  */
-function jesse_james_react_scripts() {
-  wp_enqueue_script(
-    'jesse-james-react-app', 
-    get_stylesheet_directory_uri() . '/build/index.js', 
-    array( 'wp-element', 'wp-data' ), // Add 'wp-data' here
-    '1.0.0', 
-    true 
-  );  
+function jesse_james_react_scripts()
+{
+    wp_enqueue_script(
+        'jesse-james-react-app', 
+        get_stylesheet_directory_uri() . '/build/index.js', 
+        array( 'wp-element', 'wp-data' ), // Add 'wp-data' here
+        '1.0.0', 
+        true 
+    );  
 
-  wp_enqueue_style('jesse-james-react-theme-style', get_stylesheet_uri());
+    wp_enqueue_style('jesse-james-react-theme-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'jesse_james_react_scripts');
 
 /**
  * Add pre-styles.css - for resets, etc.
  */
-add_action('wp_enqueue_scripts', function () {
-	wp_enqueue_style( 'jess-blocks-child-pre-style', get_stylesheet_directory_uri() . '/assets/css/pre-style.css' );
-});
+add_action(
+    'wp_enqueue_scripts', function () {
+        wp_enqueue_style('jess-blocks-child-pre-style', get_stylesheet_directory_uri() . '/assets/css/pre-style.css');
+    }
+);
 
 /**
  * Add Child styles.css
  */
-add_action( 'wp_enqueue_scripts', function () {
-	wp_enqueue_style( 'jess-blocks-child-style', get_stylesheet_directory_uri() . '/style.css' );
-});
+add_action(
+    'wp_enqueue_scripts', function () {
+        wp_enqueue_style('jess-blocks-child-style', get_stylesheet_directory_uri() . '/style.css');
+    }
+);
 
 /**
  * Enqueue a script
  */
-function jesse_james_enqueue_scripts() {
+function jesse_james_enqueue_scripts()
+{
     wp_enqueue_script( 
-      'jesse-james-js', 
-      get_stylesheet_directory_uri() . '/assets/js/jesse_james.js',
-      array(), 
-      '1.0.0',
-      array(
+        'jesse-james-js', 
+        get_stylesheet_directory_uri() . '/assets/js/jesse_james.js',
+        array(), 
+        '1.0.0',
+        array(
         'in_footer' => true,
         'strategy'  => 'defer',
-      )
-  );
+        )
+    );
 }
-add_action( 'wp_enqueue_scripts', 'jesse_james_enqueue_scripts' );
+add_action('wp_enqueue_scripts', 'jesse_james_enqueue_scripts');
 
 /**
  * Theme scripts and styles.
  */
-function jesse_james_blocks_scripts() {
-	wp_enqueue_style(
-		'go-child-style',
-		get_stylesheet_uri(),
-		array( 'jesse_james_blocks-style' ),
-		JESSE_JAMES_BLOCKS_VERSION
-	);
+function jesse_james_blocks_scripts()
+{
+    wp_enqueue_style(
+        'go-child-style',
+        get_stylesheet_uri(),
+        array( 'jesse_james_blocks-style' ),
+        JESSE_JAMES_BLOCKS_VERSION
+    );
 }
-add_action( 'wp_enqueue_scripts', 'jesse_james_blocks_scripts' );
+add_action('wp_enqueue_scripts', 'jesse_james_blocks_scripts');
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
@@ -121,35 +133,37 @@ add_action( 'wp_enqueue_scripts', 'jesse_james_blocks_scripts' );
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function register_block_jesse_james_block_plugin_scaffold() {
-  // Set up block directory
-  $block_dir = get_stylesheet_directory() . '/assets/blocks/';
+function register_block_jesse_james_block_plugin_scaffold()
+{
+    // Set up block directory
+    $block_dir = get_stylesheet_directory() . '/assets/blocks/';
 
-  // Block scaffold experiments
-  $block_jess_block_plugin_scaffold_experiments = $block_dir . '/jesse-james-wp-block-plugin-scaffold/jess-block-scaffold-experiments.php';
-  include_once( $block_jess_block_plugin_scaffold_experiments );
+    // Block scaffold experiments
+    $block_jess_block_plugin_scaffold_experiments = $block_dir . '/jesse-james-wp-block-plugin-scaffold/jess-block-scaffold-experiments.php';
+    include_once $block_jess_block_plugin_scaffold_experiments;
 }
-add_action( 'init', 'register_block_jesse_james_block_plugin_scaffold' );
+add_action('init', 'register_block_jesse_james_block_plugin_scaffold');
 
 /**
  * SVG URL Encode
  * 
- * @param string $svg_path Path to SVG file.
+ * @param  string $svg_path Path to SVG file.
  * @return string URL encoded SVG data.
  */
-function jj_svg_urlencode( $svg_path ) {
-	$data = file_get_contents( $svg_path );
-		// $data = preg_replace('/\v(?:[\v\h]+)/', ' ', $data);
-		$data  = str_replace( '"', "'", $data );
-		$trans = [
-			'<' => '%3c',
-			'>' => '%3e',
-			'#' => '%23',
-		];
-		$data  = strtr( $data, $trans );
-		// $data = rawurlencode($data);
-		// re-decode a few characters understood by browsers to improve compression
-		return $data;
+function jj_svg_urlencode( $svg_path )
+{
+    $data = file_get_contents($svg_path);
+    // $data = preg_replace('/\v(?:[\v\h]+)/', ' ', $data);
+    $data  = str_replace('"', "'", $data);
+    $trans = [
+    '<' => '%3c',
+    '>' => '%3e',
+    '#' => '%23',
+    ];
+    $data  = strtr($data, $trans);
+    // $data = rawurlencode($data);
+    // re-decode a few characters understood by browsers to improve compression
+    return $data;
 }//end jessejames_svg_urlencode()
 
 // echo jj_svg_urlencode( './Logo-Planck-full.svg');
@@ -157,79 +171,79 @@ function jj_svg_urlencode( $svg_path ) {
 /**
  * Get Tags by Shared Category Shortcode
  * 
- * @param array $atts Shortcode attributes.
+ * @param  array $atts Shortcode attributes.
  * @return string List of tags.
  */
 add_shortcode('category_tags_list', 'get_tags_by_shared_category_shortcode');
-function get_tags_by_shared_category_shortcode($atts) {
-  // Extract category slug from shortcode attribute (if provided)
-  $category_slug = isset($atts['category']) ? $atts['category'] : '';
+function get_tags_by_shared_category_shortcode($atts)
+{
+    // Extract category slug from shortcode attribute (if provided)
+    $category_slug = isset($atts['category']) ? $atts['category'] : '';
 
-  // Check if category slug is provided
-  if (!$category_slug) {
-    return 'Please specify the category slug using the "category" attribute.';
-  }
+    // Check if category slug is provided
+    if (!$category_slug) {
+        return 'Please specify the category slug using the "category" attribute.';
+    }
 
-  // Set up the query arguments
-  $args = array(
+    // Set up the query arguments
+    $args = array(
     'category_name' => $category_slug,
     'posts_per_page' => -1, // Get all posts in the category
-  );
+    );
 
-  // Run the query
-  $query = new WP_Query($args);
+    // Run the query
+    $query = new WP_Query($args);
 
-  // Initialize variables
-  $tags = array();
+    // Initialize variables
+    $tags = array();
 
-  // Check if posts were found
-  if ($query->have_posts()) {
-    // Loop through each post
-    while ($query->have_posts()) {
-      $query->the_post();
+    // Check if posts were found
+    if ($query->have_posts()) {
+        // Loop through each post
+        while ($query->have_posts()) {
+            $query->the_post();
 
-      // Get all tags for the current post
-      $post_tags = get_the_tags();
+            // Get all tags for the current post
+            $post_tags = get_the_tags();
 
-      // Loop through each post tag
-      if ($post_tags) {
-        foreach ($post_tags as $tag) {
-          $tag_id = $tag->term_id;
-          $tag_slug = $tag->slug;
-          $tag_name = $tag->name;
+            // Loop through each post tag
+            if ($post_tags) {
+                foreach ($post_tags as $tag) {
+                    $tag_id = $tag->term_id;
+                    $tag_slug = $tag->slug;
+                    $tag_name = $tag->name;
 
-          // Check if tag already exists in the list
-          if (!isset($tags[$tag_id])) {
-            $tags[$tag_id] = array(
-              'name' => $tag_name,
-              'url' => get_tag_link($tag_id),
-              'count' => 0,
-            );
-          }
+                    // Check if tag already exists in the list
+                    if (!isset($tags[$tag_id])) {
+                        $tags[$tag_id] = array(
+                        'name' => $tag_name,
+                        'url' => get_tag_link($tag_id),
+                        'count' => 0,
+                        );
+                    }
 
-          // Increment the count for the existing tag
-          $tags[$tag_id]['count']++;
+                    // Increment the count for the existing tag
+                    $tags[$tag_id]['count']++;
+                }
+            }
         }
-      }
     }
-  }
 
-  // Reset post data to avoid conflicts
-  wp_reset_postdata();
+    // Reset post data to avoid conflicts
+    wp_reset_postdata();
 
-  // Build the output (if tags are found)
-  $output = '';
-  if ($tags) {
-    $output .= '<ul>';
-    foreach ($tags as $tag) {
-      $output .= '<li><a href="' . $tag['url'] . '">' . $tag['name'] . ' (' . $tag['count'] . ')</a></li>';
+    // Build the output (if tags are found)
+    $output = '';
+    if ($tags) {
+        $output .= '<ul>';
+        foreach ($tags as $tag) {
+            $output .= '<li><a href="' . $tag['url'] . '">' . $tag['name'] . ' (' . $tag['count'] . ')</a></li>';
+        }
+        $output .= '</ul>';
+    } else {
+        $output .= 'No tags found for this category.';
     }
-    $output .= '</ul>';
-  } else {
-    $output .= 'No tags found for this category.';
-  }
 
-  // Return the output
-  return $output;
+    // Return the output
+    return $output;
 }
-
